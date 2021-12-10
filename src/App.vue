3c52588b703d06638cc2c2437c2ff63d3c3122d0
unknown <git@github.com:sansann2/weatherDemo.git>
2021-12-10 09:15:18 +0800

    finish project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <DisplayNumber />
+    <AddNumber />
+
+  </div>
+</template>
+
+<script>
+import AddNumber from './components/AddNumber.vue'
+import DisplayNumber from './components/DisplayNumber.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    AddNumber,
+    DisplayNumber, 
+  }
+}
+</script>
+
---
+++ src/components/DisplayNumber.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ storeState }}</h2>
+    </div>
+</template>
+
+<script>
+import {store} from '../store.js'
+
+export default {
+    data() {
+        return {
+            storeState: store.state.numbers
+        }
+    }
+}
+</script>
